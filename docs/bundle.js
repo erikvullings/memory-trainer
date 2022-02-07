663028ca213802dfe9e2a47757057aa23b54a797
Erik Vullings <erik.vullings@gmail.com>
2022-02-07 16:13:41 +0100

    Updated docs

--- docs/bundle.js
+++ docs/bundle.js
@@ -15501,7 +15501,7 @@ exports.AboutPage = void 0;
 var mithril_1 = __importDefault(__webpack_require__(1022));
 var mithril_ui_form_1 = __webpack_require__(1930);
 var models_1 = __webpack_require__(7469);
-var md = "#### Capability Assessment Tool\n\n##### Purpose\n\n##### Methodology\n...";
+var md = "#### Memory Trainer\n\nQuickly learn new words or definitions. Enter a set of words and their meaning, and the app will challenge you. When you have finished a 'round', you can start all over again, or just continue with the ones you did wrong.\n\nThis application is a digital version of the one where you write a word on one side of a card, and its definition on the other side. You stack all cards, and the learning begins when you pick your first card. Next, you read the word and guess its definition, and verify it by flipping it over. If you guessed correctly, the card goes to the correct stack, otherwise to the incorrect stack of cards. When you are done, you pickup the incorrect stack of cards and start training yourself agian.\n\nWhen you are done training in one direction (from word to definition), you can also reverse it and test yourself the other way (from definition to word).\n\nNothing is stored online, and all data is kept private locally, so you either need to download the results for later use, or use the PERMALINK option on the home page to copy a link to your clipboard, and send it to yourself. The link will also contain the data you entered, so you can also share your list of words and definitions with your friends.";
 var AboutPage = function () { return ({
     oninit: function (_a) {
         var setPage = _a.attrs.actions.setPage;
